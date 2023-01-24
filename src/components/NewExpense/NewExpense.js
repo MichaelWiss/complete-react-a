25ae7a4ca42a2c7b1719986fd1b294eed0543e0b
MichaelWiss <michael.wiss@gmail.com>
2023-01-24 11:33:47 -0600

    add className to NewExpense

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,10 @@
 import React from 'react';
+
+import ExpenseForm from './ExpenseForm';  
 import './NewExpense.css';
-import ExpenseForm  from './ExpenseForm';  
 
 const NewExpense = () => {
-    return <div>
+    return <div className='new-expense'>
        <ExpenseForm />
     </div>
 };
