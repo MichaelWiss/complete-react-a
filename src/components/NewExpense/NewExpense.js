1365240174b51f3625de4bda5dcc6bd877302e7c
MichaelWiss <michael.wiss@gmail.com>
2023-01-23 19:45:18 -0600

    form

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NewExpense.css';
+
+const NewExpense = () => {
+    return <div>
+        <form action=""></form>
+    </div>
+};
+
+export default NewExpense;
