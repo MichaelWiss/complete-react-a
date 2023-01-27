8a554549108838db5ed5b616e4750ba02cea4fe0
MichaelWiss <michael.wiss@gmail.com>
2023-01-26 18:25:09 -0600

    dumb component

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -3,16 +3,16 @@ import React from 'react';
 
 import './ExpenseFilter.css';
 
-const ExpensesFilter = () => {
+const ExpensesFilter = (props) => {
   const filterChangeHandler = (event) => {
-      console.log(event.target.value);
+      props.onChangeFilter(event.target.filter);
   };
 
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select onChange={filterChangeHandler}>
+        <select value={props.selected} onChange={filterChangeHandler}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
