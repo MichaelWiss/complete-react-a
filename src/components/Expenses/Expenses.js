b519468b31eba8dd4a71ebc0fc45a565e9109e89
MichaelWiss <michael.wiss@gmail.com>
2023-01-28 11:45:30 -0600

    filter bug

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,39 @@
 import React, { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpenseFilter from './ExpenseFilter';
 
-function Expenses(props) {
+const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2023');
 
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
       setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
-    <Card className="expenses">
-     <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-     {props.items.map(expense => (
-        <ExpenseItem  
+     <Card className="expenses">
+        <ExpenseFilter
+           selected={filteredYear} 
+           onChangeFilter={filterChangeHandler}
+          />
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem  
             key={expense.id}
             title={expense.title} 
             amount={expense.amount} 
             date={expense.date} 
          />
-         ))}
-    </Card>
+        ))}   
+        </Card>
     </div>
   );
-}
+};
 
 export default Expenses;
